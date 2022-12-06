256356d967a579740ebabd1af33838f2465f8b34
Oliver <safstrom.oliver@gmail.com>
2022-12-06 11:09:23 +0100

    connect

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+connect();
+
+    }
+private static void connect(){
+    Connection connection = null;
+    try{
+        String url = "jdbc:sqlite:/home/eox/Documents/Yrkesutbildning/ITHS/Databaser/SQLite/labb3.db";
+        connection = DriverManager.getConnection(url);
+        System.out.println("Database Connected...");
+    }catch (SQLException e){
+        System.out.println(e.getMessage());
+    }
+}
+
+
 }
