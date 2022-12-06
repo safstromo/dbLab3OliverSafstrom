26547e1e63206aeac348ec2e6e4dcb2418a1a084
Oliver <safstrom.oliver@gmail.com>
2022-12-06 13:55:46 +0100

    connect

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,50 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-connect();
 
+        selectAllFromStudio();
     }
-private static void connect(){
-    Connection connection = null;
-    try{
+
+    private static Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:/home/eox/Documents/Yrkesutbildning/ITHS/Databaser/SQLite/labb3.db";
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+        return connection;
+    }
+
+    private static Connection connect1() {
         String url = "jdbc:sqlite:/home/eox/Documents/Yrkesutbildning/ITHS/Databaser/SQLite/labb3.db";
-        connection = DriverManager.getConnection(url);
-        System.out.println("Database Connected...");
-    }catch (SQLException e){
-        System.out.println(e.getMessage());
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
-}
 
+    private static void selectAllFromStudio() {
+        String sql = "SELECT * FROM studio";
+
+        try {
+            Connection conn = connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("studioId") + "\t" +
+                        rs.getString("studioName"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
