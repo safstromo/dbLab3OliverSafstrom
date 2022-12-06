c4459ca4b158dfb3c905a713937bb7eb5fbc0f5f
Oliver <safstrom.oliver@gmail.com>
2022-12-06 15:13:44 +0100

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,71 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+	static Scanner sc = new Scanner(System.in);
 
-        selectAllFromStudio();
-    }
+	public static void main(String[] args) {
+		boolean exit = false;
+		selectAllFromStudio();
 
-    private static Connection connect() {
-        Connection connection = null;
-        try {
-            String url = "jdbc:sqlite:src/db/labb3.db";
-            connection = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+		while (!exit) {
+			String menuInput = sc.nextLine();
 
-        }
-        return connection;
-    }
+			printMenu();
+			switch (menuInput){
+				case "1" -> add();
+				case "2" -> show();
+				case "3" -> update();
+				case "4" -> remove();
+				case "e,E" -> exit = true;
+			}
+		}
 
-    private static void selectAllFromStudio() {
-        String sql = "SELECT * FROM studio";
+	}
 
-        try (Connection conn = connect();
-             Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
+	private static void add(){}
 
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getInt("studioId") + "\t" +
-                        rs.getString("studioName"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+	private static void show() {}
+	private static void update(){}
+	private static void remove(){}
+
+	private static Connection connect() {
+		Connection connection = null;
+		try {
+			String url = "jdbc:sqlite:src/db/labb3.db";
+			connection = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		}
+		return connection;
+	}
+
+	private static void selectAllFromStudio() {
+		String sql = "SELECT * FROM studio";
+
+		try (Connection conn = connect();
+			 Statement stmt = conn.createStatement()) {
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// loop through the result set
+			while (rs.next()) {
+				System.out.println(rs.getInt("studioId") + "\t" +
+						rs.getString("studioName"));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void printMenu() {
+		System.out.println("""
+				1. Add
+				2. Show
+				3. Update
+				4. Remove
+				e. Exit
+				""");
+	}
 
 }
